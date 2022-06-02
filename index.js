efb07c2228fef532be3030378bfa51d86134b3e8
Pedro Candido <pedrohcn02@gmail.com>
2022-06-02 11:02:36 -0300

    a

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('boa tarde')
+  res.send(process.env.TESTE)
 })
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log(`fazendo traquinagens na porta ${PORT}, ${process.env.TESTE}`)
+  console.log(`fazendo traquinagens na porta ${PORT}`)
 })
